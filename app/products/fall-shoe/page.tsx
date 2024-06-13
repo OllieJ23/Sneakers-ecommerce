f07fa66a09a4e30500a9d17d5ab5d2563e5f977e
Ollie J <oliver.jeremiah230@gmail.com>
2024-06-13 12:00:10 +0100

    Updated Thumbnails Path

--- app/products/fall-shoe/page.tsx
+++ app/products/fall-shoe/page.tsx
@@ -9,10 +9,10 @@ function page() {
     <div className="mt-8 flex flex-col items-center justify-center gap-10 sm:flex-row lg:gap-40">
       <ShoePics
         thumbnailURLs={[
-          "/images/image-product-1-thumbnail.jpg",
-          "/images/image-product-2-thumbnail.jpg",
-          "/images/image-product-3-thumbnail.jpg",
-          "/images/image-product-4-thumbnail.jpg",
+          "/images/product-1-images/image-product-1-thumbnail.jpg",
+          "/images/product-1-images/image-product-2-thumbnail.jpg",
+          "/images/product-1-images/image-product-3-thumbnail.jpg",
+          "/images/product-1-images/image-product-4-thumbnail.jpg",
         ]}
         textID={fallShoeData.productId}
         imgType="jpg"
